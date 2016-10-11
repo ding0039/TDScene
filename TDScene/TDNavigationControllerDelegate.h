1920bfc65254b5ad85bf8eb6d4da242e351d6161
Terence <terence@TerencedeiMac.local>
2016-10-11 16:59:22 +0800

    自定义Storyboard上的push动画

---
+++ TDScene/TDNavigationControllerDelegate.h
@@ -0,0 +1,13 @@
+//
+//  TDNavigationControllerDelegate.h
+//  TDScene
+//
+//  Created by Terence on 2016/10/11.
+//  Copyright © 2016年 Terence. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TDNavigationControllerDelegate : NSObject
+
+@end
---
+++ TDScene/TDNavigationControllerDelegate.m
@@ -0,0 +1,13 @@
+//
+//  TDNavigationControllerDelegate.m
+//  TDScene
+//
+//  Created by Terence on 2016/10/11.
+//  Copyright © 2016年 Terence. All rights reserved.
+//
+
+#import "TDNavigationControllerDelegate.h"
+
+@implementation TDNavigationControllerDelegate
+
+@end
